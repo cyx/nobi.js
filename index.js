ac80d6e56538719a73bd9e63a47cea142d99aec2
Cyril David <cyx@cyx.is>
2013-10-09 19:35:44 -0700

    Add timestamp signer

--- index.js
+++ index.js
@@ -2,29 +2,32 @@ var scmp = require('scmp');
 var crypto = require('crypto');
 var util = require('util');
 
-/**
- *  Usage:
- *
- *      var signer = nobi('my secret');
- *      signer.sign('string-to-sign');
- *
- *  Options:
- *
- *    opts.salt (default nobi.Signer)
- *    opts.sep (default '.')
- *    opts.digestMethod (default sha1)
-*/
+// This is used as peg so the timestamps are smaller
+// than what they need to be.
+var EPOCH = 1293840000; // 2011/01/01 in UTC
+
+// Usage:
+//
+//     var signer = nobi('my secret');
+//     signer.sign('string-to-sign');
+//
+// Options:
+//
+//   opts.salt (default nobi.Signer)
+//   opts.sep (default '.')
+//   opts.digestMethod (default sha1)
+//
 function nobi(secret, opts) {
     opts = opts || {};
 
     var salt = opts.salt || 'nobi.Signer';
     var sep = opts.sep || '.';
     var digestMethod = opts.digestMethod || 'sha1';
-    var algorithm = HMACAlgorithm(digestMethod);
+    var algorithm = hmacAlgorithm(digestMethod);
 
     var self = {
         sign: function (value) {
-            return [value, sep, signature(value)].join('');
+            return [value, sep, this._signature(value)].join('');
         },
 
         unsign: function (data) {
@@ -37,29 +40,92 @@ function nobi(secret, opts) {
             var value = data.slice(0, index);
             var sig = data.slice(index + 1, data.length);
 
-            if (scmp(sig, signature(value))) {
+            if (scmp(sig, this._signature(value))) {
                 return value;
             }
 
             fail('BadSignature: Signature %s does not match', sig);
+        },
+
+        _deriveKey: function () {
+            return algorithm(secret, salt);
+        },
+
+        _signature: function (value) {
+            var key = this._deriveKey();
+            var sig = algorithm(key, value);
+
+            return new Buffer(sig).toString('base64');
         }
     };
 
-    function deriveKey() {
-        return algorithm(secret, salt);
-    }
+    return self;
 
-    function signature(value) {
-        key = deriveKey();
-        sig = algorithm(secret, salt);
+}
 
-        return new Buffer(sig).toString('base64');
-    }
+// Usage:
+//
+//     var signer = nobi.timestampSigner('my secret');
+//     signer.sign('string-to-sign');
+//
+// Options:
+//
+//   opts.salt (default nobi.TimestampSigner)
+//   opts.sep (default '.')
+//   opts.digestMethod (default sha1)
+//
+function timestampSigner(secret, opts) {
+    opts = opts || {};
+
+    var salt = opts.salt || 'nobi.TimestampSigner';
+    var sep = opts.sep || '.';
+    var digestMethod = opts.digestMethod || 'sha1';
+
+    var signer = nobi(secret, {
+        salt: salt,
+        sep: sep,
+        digestMethod: digestMethod
+    });
+
+    var self = {
+        sign: function (value) {
+            var timestamp = b64encode(String(getTimestamp()));
+
+            value = [value, sep, timestamp].join('');
+
+            return [value, sep, signer._signature(value)].join('');
+        },
+
+        unsign: function (data, opts) {
+            var maxAge = (opts && opts.maxAge) || 60000;
+            var unsigned = signer.unsign(data);
+            var index = unsigned.lastIndexOf(sep);
+
+            if (index === -1) {
+                fail('BadSignature: No %s found in value', sep);
+            }
+
+            var value = unsigned.slice(0, index);
+            var ts = unsigned.slice(index + 1, unsigned.length);
+            var timestamp = Number(b64decode(ts));
+            var age = getTimestamp() - timestamp;
+
+            if (age > maxAge) {
+                fail('BadSignature: Signature Expired', value);
+            }
+
+            return value;
+        }
+    };
 
     return self;
 }
 
-function HMACAlgorithm(digestMethod) {
+function getTimestamp() {
+    return Date.now() - EPOCH;
+}
+
+function hmacAlgorithm(digestMethod) {
     function signature(key, data) {
         var hmac = crypto.createHmac(digestMethod, key);
 
@@ -71,9 +137,19 @@ function HMACAlgorithm(digestMethod) {
     return signature;
 }
 
+function b64encode(string) {
+    return new Buffer(string).toString('base64');
+}
+
+function b64decode(string) {
+    return new Buffer(string, 'base64').toString();
+}
+
 function fail() {
     throw new Error(util.format.apply(null, arguments));
 }
+
+nobi.timestampSigner = timestampSigner;
 
 module.exports = nobi;
 
--- test/nobi.js
+++ test/nobi.js
@@ -11,7 +11,6 @@ test('nobi.signer', function (t) {
 
 test('nobi.signer invalid string', function (t) {
     var signer = nobi('my secret');
-    var signed = signer.sign('1');
 
     t.plan(1);
 
@@ -24,7 +23,7 @@ test('nobi.signer invalid string', function (t) {
         err = e;
     }
 
-    t.ok(/Error: BadSignature/.test(err), 'exception raised');
+    t.ok(/Error: BadSignature/.test(err), 'bad signature check');
 });
 
 test('nobi.signer tampered signed string', function (t) {
@@ -42,6 +41,30 @@ test('nobi.signer tampered signed string', function (t) {
         err = e;
     }
 
-    t.ok(/Error: BadSignature/.test(err), 'exception raised');
+    t.ok(/Error: BadSignature/.test(err), 'tampered: bad signature');
+});
+
+test('nobi.timestamp signer', function (t) {
+    var signer = nobi.timestampSigner('my secret');
+    var signed = signer.sign('1');
+
+    t.plan(1);
+
+    t.equal('1', signer.unsign(signed, { maxAge: 1 }));
+});
+
+test('nobi.timestamp signer expired', function (t) {
+    var signer = nobi.timestampSigner('my secret');
+    var signed = signer.sign('1');
+
+    t.plan(1);
+
+    setTimeout(function () {
+        try {
+            t.equal('1', signer.unsign(signed, { maxAge: 1 }));
+        } catch (err) {
+            t.ok(/Error: BadSignature/.test(err), 'expired');
+        }
+    }, 5);
 });
 
